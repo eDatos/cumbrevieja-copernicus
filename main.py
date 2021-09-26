5775a862d12d9e81957630ec76736ee7e8a83d94
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-09-26 09:54:37 +0100

    Remove download dir after processing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import shutil
+
 import scrap
 import services
+import settings
 
 if links := scrap.get_links():
     vectors_url, pdf_url = links
     scrap.download_vectors(vectors_url)
     pdf_file = scrap.download_pdf(pdf_url)
     map_timestamp = services.extract_map_timestamp(pdf_file)
-    print(map_timestamp)
+    shutil.rmtree(settings.DOWNLOADS_DIR)
