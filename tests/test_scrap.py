126d1c6e59acab24aa5b3def60e6e84c546f132e
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-10-05 10:26:21 +0100

    Fix tests

--- tests/test_scrap.py
+++ tests/test_scrap.py
@@ -1,15 +1,9 @@
 import shutil
 from pathlib import Path
 
-import pytest
 import settings
 from bs4.element import Tag
 from copernicus import scrap
-
-
-@pytest.fixture
-def products():
-    return list(scrap.get_products())
 
 
 def test_get_products(products):
@@ -22,38 +16,44 @@ def test_get_products(products):
 
 def test_links_available(products):
     # [EMSR546] La Palma: Grading Product, Monitoring 1, version 3, release 1, RTP Map #02
-    product, monitoring_id = products[9]
-    assert monitoring_id == 1
-    vectors_url, pdf_url = scrap.get_links(product)
-    assert (
-        vectors_url == 'https://emergency.copernicus.eu/mapping/download/'
-        '189298/EMSR546_AOI01_GRA_MONIT01_r1_RTP02_v3_vector.zip'
-    )
-    assert (
-        pdf_url == 'https://emergency.copernicus.eu/mapping/system/files/components/'
-        'EMSR546_AOI01_GRA_MONIT01_r1_RTP02_v3.pdf'
-    )
+    for product, monitoring_id in products:
+        if monitoring_id == 1:
+            vectors_url, pdf_url = scrap.get_links(product)
+            assert (
+                vectors_url == 'https://emergency.copernicus.eu/mapping/download/'
+                '189298/EMSR546_AOI01_GRA_MONIT01_r1_RTP02_v3_vector.zip'
+            )
+            assert (
+                pdf_url
+                == 'https://emergency.copernicus.eu/mapping/system/files/components/'
+                'EMSR546_AOI01_GRA_MONIT01_r1_RTP02_v3.pdf'
+            )
+            break
 
 
 def test_links_not_available(products):
     # [EMSR546] La Palma: Grading Product, Monitoring 7, version 2, release 1, RTP Map #02
-    product, monitoring_id = products[3]
-    assert monitoring_id == 7
-    vectors_url, pdf_url = scrap.get_links(product)
-    assert vectors_url is None
-    assert (
-        pdf_url == 'https://emergency.copernicus.eu/mapping/system/files/components/'
-        'EMSR546_AOI01_GRA_MONIT07_r1_RTP02_v2.pdf'
-    )
+    for product, monitoring_id in products:
+        if monitoring_id == 7:
+            vectors_url, pdf_url = scrap.get_links(product)
+            assert vectors_url is None
+            assert (
+                pdf_url
+                == 'https://emergency.copernicus.eu/mapping/system/files/components/'
+                'EMSR546_AOI01_GRA_MONIT07_r1_RTP02_v2.pdf'
+            )
+            break
 
 
-def test_download_vectors():
+def test_download_vectors(map_timestamp_m1):
     url = (
         'https://emergency.copernicus.eu/mapping/download/'
         '189298/EMSR546_AOI01_GRA_MONIT01_r1_RTP02_v3_vector.zip'
     )
-    scrap.download_vectors(url, 1)
-    f = Path(settings.DOWNLOADS_DIR / 'EMSR546-M1.zip')
+    scrap.download_vectors(url, 1, map_timestamp_m1)
+    component_id = settings.COPERNICUS_COMPONENT_ID
+    filename = f'20210921_0714_UTC-{component_id}-M1.zip'
+    f = Path(settings.DOWNLOADS_DIR / filename)
     assert f.exists()
     shutil.rmtree(f.parent)
 
@@ -64,6 +64,8 @@ def test_download_pdf():
         'EMSR546_AOI01_GRA_MONIT01_r1_RTP02_v3.pdf'
     )
     scrap.download_pdf(url, 1)
-    f = Path(settings.DOWNLOADS_DIR / 'EMSR546-M1.pdf')
+    component_id = settings.COPERNICUS_COMPONENT_ID
+    filename = f'{component_id}-M1.pdf'
+    f = Path(settings.DOWNLOADS_DIR / filename)
     assert f.exists()
     shutil.rmtree(f.parent)
--- tests/test_services.py
+++ tests/test_services.py
@@ -4,12 +4,19 @@ import settings
 from copernicus import scrap, services
 
 
-def test_extract_map_timemstamp():
+def test_extract_map_timemstamp(map_timestamp_m1):
     pdf_url = (
         'https://emergency.copernicus.eu/mapping/system/files/components/'
         'EMSR546_AOI01_GRA_MONIT01_r1_RTP02_v3.pdf'
     )
     pdf_file = scrap.download_pdf(pdf_url, 1)
-    map_timestamp = services.extract_map_timestamp(pdf_file)
-    assert map_timestamp == ('21/09/2021 07:14 ' + settings.MAP_TIMESTAMP_TZ)
+    map_timestamp_to_be_checked = services.extract_map_timestamp(pdf_file)
+    assert map_timestamp_m1 == map_timestamp_to_be_checked
     shutil.rmtree(pdf_file.parent)
+
+
+def test_build_vectors_filename(map_timestamp_m1):
+    vectors_filename_to_be_checked = services.build_vectors_filename(1, map_timestamp_m1)
+    component_id = settings.COPERNICUS_COMPONENT_ID
+    vectors_filename = f'20210921_0714_UTC-{component_id}-M1.zip'
+    assert vectors_filename == vectors_filename_to_be_checked
